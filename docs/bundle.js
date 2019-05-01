0ffdddaaf328f9462711cc6199e683e16b0a9ecc
Ben Newman <ben@benjamn.com>
2019-04-30 20:15:16 -0400

    Regenerate docs/bundle.js with latest implementation.
    Command:
    
      npx browserify -s regenerator ./main.js > docs/bundle.js

--- docs/bundle.js
+++ docs/bundle.js
@@ -158,7 +158,6 @@ var fs = require("fs");
 var through = require("through");
 var transform = require("./lib/visit").transform;
 var utils = require("./lib/util");
-var genOrAsyncFunExp = /\bfunction\s*\*|\basync\b/;
 
 function exports(file, options) {
   var data = [];
@@ -213,12 +212,10 @@ function compile(source, options) {
     includeRuntime: false
   });
 
-  // Shortcut: Transform only if generators or async functions present.
-  if (genOrAsyncFunExp.test(source)) {
-    result = require("@babel/core").transformSync(source, transformOptions);
-  } else {
-    result = { code: source };
-  }
+  var result = require("@babel/core").transformSync(
+    source,
+    transformOptions
+  );
 
   if (options.includeRuntime === true) {
     result.code = getRuntimeCode() + "\n" + result.code;
