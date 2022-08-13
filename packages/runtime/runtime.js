94ebe534998d7c85dc8c3b4967209619683710f9
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-08-13 12:37:00 +0200

    Rename `define`->`defineProperty`

--- packages/runtime/runtime.js
+++ packages/runtime/runtime.js
@@ -10,7 +10,7 @@ var runtime = (function (exports) {
 
   var Op = Object.prototype;
   var hasOwn = Op.hasOwnProperty;
-  var define = Object.defineProperty || function (obj, key, desc) { obj[key] = desc.value; };
+  var defineProperty = Object.defineProperty || function (obj, key, desc) { obj[key] = desc.value; };
   var undefined; // More compressible than void 0.
   var $Symbol = typeof Symbol === "function" ? Symbol : {};
   var iteratorSymbol = $Symbol.iterator || "@@iterator";
@@ -43,7 +43,7 @@ var runtime = (function (exports) {
 
     // The ._invoke method unifies the implementations of the .next,
     // .throw, and .return methods.
-    define(generator, "_invoke", { value: makeInvokeMethod(innerFn, self, context) });
+    defineProperty(generator, "_invoke", { value: makeInvokeMethod(innerFn, self, context) });
 
     return generator;
   }
@@ -215,7 +215,7 @@ var runtime = (function (exports) {
 
     // Define the unified helper method that is used to implement .next,
     // .throw, and .return (see defineIteratorMethods).
-    define(this, "_invoke", { value: enqueue });
+    defineProperty(this, "_invoke", { value: enqueue });
   }
 
   defineIteratorMethods(AsyncIterator.prototype);
